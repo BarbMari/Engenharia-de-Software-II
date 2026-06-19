06182cc6a6ea2b8c168460b7890e9753e5fdacc6
Laura <laurarobettegrazziolli@gmail.com>
2026-06-19 17:19:05 -0300

    masi errinhos no conection

--- Administrador/Js/conection.php
+++ Administrador/Js/conection.php
@@ -1,21 +1,23 @@
 <?php
     class connection {
-        private $username='root';
-        private $password='';
-        private $dbname='pizzaria';
-        private $host='localhost';
-        private $charset='utf8mb4';
-
+        private $username = 'root';
+        private $password = '';
+        private $dbname = 'pizzaria';
+        private $host = 'localhost';
+        private $charset = 'utf8mb4';
+ 
         public function connect() {
             try {
-                $dns = "mysql:host=$this->host;dbname=$this->$dbname;charset=$this->$charset;";
+                $dsn = "mysql:host={$this->host};dbname={$this->dbname};charset={$this->charset};";
                 $options = [
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES   => false,
                 ];
-                return new PDO($dns, $this->username, $this->password, $options);
-            } catch (PDOException $E) {die("A conexão fez 'capuft' \n" . $e->getMessage());}
+                return new PDO($dsn, $this->username, $this->password, $options);
+            } catch (PDOException $e) {
+                die("A conexão fez 'capuft' \n" . $e->getMessage());
+            }
         }
     }
 ?>
