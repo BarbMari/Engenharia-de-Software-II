accd44355233209febabd97f06fb21415db8b4ba
Laura <laurarobettegrazziolli@gmail.com>
2026-06-19 17:15:51 -0300

    errinhos no conection

--- Administrador/Js/conection.php
+++ Administrador/Js/conection.php
@@ -8,13 +8,13 @@
 
         public function connect() {
             try {
-                $dns = "mysql:host=$this->host;dmname=$this->$dbname;charset=$this->$charset;";
+                $dns = "mysql:host=$this->host;dbname=$this->$dbname;charset=$this->$charset;";
                 $options = [
                     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES   => false,
                 ];
-                return new PDO($dns, $this->user, $this->password, $options);
+                return new PDO($dns, $this->username, $this->password, $options);
             } catch (PDOException $E) {die("A conexão fez 'capuft' \n" . $e->getMessage());}
         }
     }
